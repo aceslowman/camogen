a000e50b593c4feefe2cb5ed4c1068a8d19ba7bc
Austin Lee Slominski <austin.slominski@gmail.com>
2019-12-19 15:07:00 -0700

    Update Parameter.js

--- src/components/Parameter.js
+++ src/components/Parameter.js
@@ -2,32 +2,18 @@ import React from 'react';
 
 import { observer } from 'mobx-react';
 
-import MainContext from '../MainContext';
-
-const style = {};
-
-const Parameter = observer(class Parameter extends React.Component {
-
-    static contextType = MainContext;
-
-    render() {
-        const store = this.context.store;
-        const data = this.props.data;
-
-        return(
-            <fieldset>
-                <legend>{data.name}</legend>
-                <input 
-                    style={{...this.props.style,...style}}
-                    type="number" 
-                    step={0.1}
-                    placeholder={data.name}
-                    value={data.value}
-                    onChange={(e) => data.value = e.target.value}
-                />
-            </fieldset>              
-        )
-    }
-});
+const Parameter = observer(({style, data}) => (
+    <fieldset>
+        <legend>{data.name}</legend>
+        <input 
+            style={{...style}}
+            type="number" 
+            step={0.1}
+            placeholder={data.name}
+            value={data.value}
+            onChange={(e) => data.value = e.target.value}
+        />
+    </fieldset>              
+));
 
 export default Parameter;
