e1844f738444517cdc22f1cb4886eeb7de69c36e
Austin Lee Slominski <austin.slominski@gmail.com>
2019-12-19 12:56:08 -0700

    Create UV.js

---
+++ src/components/nodes/shaders/UV.js
@@ -0,0 +1,37 @@
+const UV = {
+	name: 'UV',
+	uniforms: {
+      bSquare: true,
+    },  
+	precision: `
+		#ifdef GL_ES
+		precision highp float;
+		#endif 
+	`,
+	vert: `
+    attribute vec3 aPosition;
+    attribute vec2 aTexCoord;
+    varying vec2 vTexCoord;
+    void main() {
+        vTexCoord = aTexCoord;
+        vec4 positionVec4 = vec4(aPosition,1.0);
+        positionVec4.xy = positionVec4.xy * vec2(1.,-1.);
+        gl_Position = positionVec4;
+    }
+	`,
+	frag: `
+    varying vec2 vTexCoord;
+    uniform sampler2D tex0;
+    uniform vec2 resolution;
+    uniform bool bSquare;
+    void main() {
+        vec3 color = vec3(0.0);
+        float aspect = resolution.y/resolution.x;
+        vec2 uv = vTexCoord;
+        if(bSquare) {
+        uv.y *= aspect;	    
+        }
+        gl_FragColor = vec4(uv.x,uv.y,1.0,1.0);
+    }
+	`
+};
