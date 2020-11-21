74f16b87e367c8522c2337166adf60aacc16e932
Glitch (maize-indecisive-dandelion) <none>
2020-11-21 06:02:19 +0000

    🍄⛽️ Checkpoint ./src/components/panels/ShaderGraphComponent.js:24872531/17101

--- src/components/panels/ShaderGraphComponent.js
+++ src/components/panels/ShaderGraphComponent.js
@@ -1,96 +1,109 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import MainContext from '../../MainContext';
-import GraphComponent from '../graph/GraphComponent';
-import { GenericPanel } from 'maco-ui';
-import { observer } from 'mobx-react';
+import React, { useContext, useEffect, useState, useRef } from "react";
+import MainContext from "../../MainContext";
+import GraphComponent from "../graph/GraphComponent";
+import { GenericPanel } from "maco-ui";
+import { observer } from "mobx-react";
 
-const ShaderGraph = observer((props) => {
-	const store = useContext(MainContext).store;
-	const [useKeys, setUseKeys] = useState(false);
+const ShaderGraph = observer(props => {
+  const store = useContext(MainContext).store;
+  const [useKeys, setUseKeys] = useState(false);
   const mainRef = useRef();
 
-	const handleFocus = (e) => {
-		setUseKeys(e ? true : false);
-	}
+  const handleFocus = e => {
+    setUseKeys(e ? true : false);
+  };
 
-	useEffect(()=>{
-		if(useKeys) {
-			store.context.setKeymap({
-				"ArrowUp": () => {
-					if (props.selectedNode && props.selectedNode.parents.length)
-						props.selectedNode.parents[0].select()						
-				},
-				"ArrowDown": () => {
-					if (props.selectedNode && props.selectedNode.children.length)
-						props.selectedNode.children[0].select()						
-				},
-				"ArrowLeft": () => {
-					if (props.selectedNode && props.selectedNode.children.length) {
-						let idx = props.selectedNode.children[0].parents.indexOf(props.selectedNode);
-						idx--;
+  useEffect(() => {
+    if (useKeys) {
+      store.context.setKeymap({
+        ArrowUp: () => {
+          if (props.selectedNode && props.selectedNode.parents.length)
+            props.selectedNode.parents[0].select();
+        },
+        ArrowDown: () => {
+          if (props.selectedNode && props.selectedNode.children.length)
+            props.selectedNode.children[0].select();
+        },
+        ArrowLeft: () => {
+          if (props.selectedNode && props.selectedNode.children.length) {
+            let idx = props.selectedNode.children[0].parents.indexOf(
+              props.selectedNode
+            );
+            idx--;
 
-						if (idx >= 0) {
-							props.selectedNode.children[0].parents[idx].select();
-						}
-					}
-				},
-				"ArrowRight": () => {
-					if (props.selectedNode && props.selectedNode.children.length) {
-						let idx = props.selectedNode.children[0].parents.indexOf(props.selectedNode);
-						idx++;
+            if (idx >= 0) {
+              props.selectedNode.children[0].parents[idx].select();
+            }
+          }
+        },
+        ArrowRight: () => {
+          if (props.selectedNode && props.selectedNode.children.length) {
+            let idx = props.selectedNode.children[0].parents.indexOf(
+              props.selectedNode
+            );
+            idx++;
 
-						if (idx <= props.selectedNode.children[0].parents.length - 1)
-							props.selectedNode.children[0].parents[idx].select();
-					}
-				},
-				"Delete": () => {
-					props.data.removeSelected();
-				}
-			})
-		} else {
-			store.context.removeKeymap();
-		}
-	}, [
-		props.selectedNode,
-		props.data,
-		store.context,
-		useKeys
-	])
-	
-	const handleContextMenu = (e) => {
-    console.log('shader graph context', e.target)
-    console.log('mainref',mainRef.current)
-		// e.preventDefault();
-		// e.stopPropagation();
+            if (idx <= props.selectedNode.children[0].parents.length - 1)
+              props.selectedNode.children[0].parents[idx].select();
+          }
+        },
+        Delete: () => {
+          props.data.removeSelected();
+        }
+      });
+    } else {
+      store.context.removeKeymap();
+    }
+  }, [props.selectedNode, props.data, store.context, useKeys]);
+
+  const handleContextMenu = e => {
+    console.log("shader graph context", e.currentTarget);
+    console.log(e.target)
+    console.log(e)
+    console.log("mainref", mainRef.current);
+    // e.preventDefault();
+    // e.stopPropagation();
     
-    if(e.target === mainRef.current)
-      store.context.setContextmenu([{
-        label: "Clear",
-        onClick: () => store.scene.clear()
-      }])
-	}
+    console.log('bubbles',e.bubbles)
 
-	return(	
-		<GenericPanel 
+    if (e.target === mainRef.current)
+      store.context.setContextmenu([
+        {
+          label: "Clear",
+          onClick: () => store.scene.clear()
+        }
+      ]);
+  };
+
+  return (
+    <GenericPanel
       onRef={mainRef}
-			panel={props.panel}
-			onContextMenu={handleContextMenu}
-			onFocus={handleFocus}
-			indicators={useKeys ? [
-				{label:'k', color: store.theme.accent_color, title: 'Keybind Focus'}
-			] : null}
-		>		
-			{props.data && (
-				<GraphComponent 
-					data={props.data}
-					coord_bounds={props.coord_bounds}
-					selectedNode={props.selectedNode}
-				/>
-			)}
+      panel={props.panel}
+      onContextMenuCapture={handleContextMenu}
+      onFocus={handleFocus}
+      indicators={
+        useKeys
+          ? [
+              {
+                label: "k",
+                color: store.theme.accent_color,
+                title: "Keybind Focus"
+              }
+            ]
+          : null
+      }
+    >
+      {props.data && (
+        <GraphComponent
+          data={props.data}
+          coord_bounds={props.coord_bounds}
+          selectedNode={props.selectedNode}
+        />
+      )}
 
-			{ props.data && props.data.updateFlag }
-		</GenericPanel>
-	)  
+      {props.data && props.data.updateFlag}
+    </GenericPanel>
+  );
 });
 
 export default ShaderGraph;
