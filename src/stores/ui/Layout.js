1a7760f277bf48ac8511a66265d4531057e69a06
Glitch (maize-indecisive-dandelion) <none>
2020-11-26 00:07:00 +0000

    🏋🦁 Checkpoint ./src/stores/ui/Layout.js:24872531/1226

--- src/stores/ui/Layout.js
+++ src/stores/ui/Layout.js
@@ -39,12 +39,12 @@ export const CoreLayouts = {
     }
   },
   SHADER_EDIT: {
-    id: "main",
+    id: "SHADER_EDIT",
     direction: "HORIZONTAL",
     size: 1,
     children: [
       {
-        id: "shader_editor",
+        id: "SHADER_EDIT_shader_editor",
         panel: "shader_editor",
         size: 2 / 3
       },
@@ -53,12 +53,12 @@ export const CoreLayouts = {
         direction: "VERTICAL",
         children: [
           {
-            id: "shader_graph",
+            id: "SHADER_EDIT_shader_graph",
             panel: "shader_graph",
             size: 2 / 3
           },
           {
-            id: "messages",
+            id: "SHADER_EDIT_messages",
             panel: "messages"
           }
         ]
@@ -66,53 +66,53 @@ export const CoreLayouts = {
     ],
     panels: {
       shader_graph: {
-        id: "shader_graph",
+        id: "SHADER_EDIT_shader_graph",
         ...CorePanels["SHADER_GRAPH"]        
       },
       messages: {
-        id: "messages",
+        id: "SHADER_EDIT_messages",
         ...CorePanels["MESSAGES"]        
       },
       shader_editor: {
-        id: "shader_editor",
+        id: "SHADER_EDIT_shader_editor",
         ...CorePanels["SHADER_EDITOR"]        
       }
     }
   },
   SHADER_CONTROL: {
-    id: "main",
+    id: "SHADER_CONTROL",
     direction: "HORIZONTAL",
     size: 1,
     children: [
       {
-        id: "shader_graph",
-        panel: "shader_graph_panel",
+        id: "shader_graph_panel",
+        panel: "SHADER_CONTROL_shader_graph_panel",
         size: 1 / 3
       },
       {
         id: "shader_controls_panel",
-        panel: "shader_controls"
+        panel: "SHADER_CONTROL_shader_controls"
       }
     ],
     panels: {
       shader_graph: {
-        id: "shader_graph_panel",        
+        id: "SHADER_CONTROL_shader_graph_panel",        
         ...CorePanels["SHADER_GRAPH"] 
       },
       shader_controls: {
-        id: "shader_controls_panel",        
+        id: "SHADER_CONTROL_shader_controls_panel",        
         ...CorePanels["SHADER_CONTROLS"] 
       }
     }
   },
   DEBUG: {
-    id: "main",
+    id: "DEBUG",
     direction: "HORIZONTAL",
     size: 1,
     children: [
       {
         id: "debug",
-        panel: "debug",
+        panel: "DEBUG_debug",
         size: 2 / 3
       },
       {
@@ -121,49 +121,28 @@ export const CoreLayouts = {
         children: [
           {
             id: "shader_graph",
-            panel: "shader_graph",
+            panel: "DEBUG_shader_graph",
             size: 2 / 3
           },
           {
             id: "messages",
-            panel: "messages"
+            panel: "DEBUG_messages"
           }
         ]
       }
     ],
     panels: {
       shader_graph: {
-        id: "shader_graph",
-        type: "SHADER_GRAPH",
-        title: "Shader Graph",
-        floating: false,
-        canFloat: true,
-        defaultWidth: 100,
-        defaultHeight: 200,
-        dimensions: [100, 100],
-        position: [100, 150]
+        id: "DEBUG_shader_graph_panel",
+        ...CorePanels["SHADER_GRAPH"] 
       },
       messages: {
-        id: "messages",
-        type: "MESSAGES",
-        title: "Messages",
-        floating: false,
-        canFloat: true,
-        defaultWidth: 100,
-        defaultHeight: 200,
-        dimensions: [100, 100],
-        position: [100, 150]
+        id: "DEBUG_messages_panel",
+        ...CorePanels["MESSAGES"] 
       },
       debug: {
-        id: "debug",
-        type: "DEBUG",
-        title: "Debug",
-        floating: false,
-        canFloat: true,
-        defaultWidth: 100,
-        defaultHeight: 200,
-        dimensions: [100, 100],
-        position: [100, 150]
+        id: "DEBUG_debug_panel",
+        ...CorePanels["DEBUG"] 
       }
     }
   },
