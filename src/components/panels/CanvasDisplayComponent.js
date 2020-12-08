515302a5c54e11f2437c551997bd2988e68d8314
Glitch (maize-indecisive-dandelion) <none>
2020-12-08 22:58:23 +0000

    ⏰🎄 Checkpoint ./src/components/panels/CanvasDisplayComponent.js:24872531/323

--- src/components/panels/CanvasDisplayComponent.js
+++ src/components/panels/CanvasDisplayComponent.js
@@ -20,12 +20,18 @@ const CanvasDisplay = observer(props => {
 
     let inner_bounds = wrapper_ref.current.getBoundingClientRect();
     let panel_bounds = panel_ref.current.parentElement.getBoundingClientRect();    
+    console.log('inner', wrapper_ref.current)
+    console.log('panelele', panel_ref.current.parentElement)
   console.log('inner_bounds', inner_bounds)
     console.log('panel', panel_bounds)
+    
+    let offset_x = panel_bounds.width - inner_bounds.width;
+    let offset_y = panel_bounds.height - inner_bounds.height;
+    
     // let w = Math.floor(inner_bounds.width - (panel_bounds.width - inner_bounds.width));
     // let h = Math.floor(inner_bounds.height - (panel_bounds.height - inner_bounds.height));
-    let w = Math.floor(inner_bounds.width);
-    let h = Math.floor(inner_bounds.height);
+    let w = Math.floor(inner_bounds.width+offset_x);
+    let h = Math.floor(inner_bounds.height+offset_y);
     store.resizeCanvas(w, h);
 
     setWidth(w);
