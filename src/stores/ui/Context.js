767728163918979daf7bffd36df8fddf67dd7c20
Glitch (camogen) <none>
2020-12-10 19:39:09 +0000

    💳🌕 Checkpoint ./src/stores/ui/Context.js:24872531/1243

--- src/stores/ui/Context.js
+++ src/stores/ui/Context.js
@@ -1,24 +1,23 @@
 import { types } from "mobx-state-tree";
-import tinykeys from 'tinykeys';
+import tinykeys from "tinykeys";
+import { observable } from 'mobx';
 
 const Context = types
-    .model('Context', {
-        
-    })
-    .volatile(self => ({
-        contextmenu: {},
-        keylistener: tinykeys(window, {}),
-        keymap: null,
-    }))
-    .actions(self => ({
-        setKeymap: keymap => {
-            if(self.keylistener) self.keylistener();
-            
-            self.keymap = keymap;
-            self.keylistener = tinykeys(window, self.keymap);   
-        },
-        removeKeymap: () => self.keylistener(),
-        setContextmenu: c => self.contextmenu = c
-    }))
+  .model("Context", {})
+  .volatile(self => ({
+    contextmenu: observable.deep({}),
+    keylistener: tinykeys(window, {}),
+    keymap: null
+  }))
+  .actions(self => ({
+    setKeymap: keymap => {
+      if (self.keylistener) self.keylistener();
+
+      self.keymap = keymap;
+      self.keylistener = tinykeys(window, self.keymap);
+    },
+    removeKeymap: () => self.keylistener(),
+    setContextmenu: c => (self.contextmenu = c)
+  }));
 
 export default Context;
