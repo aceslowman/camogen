b5513c4b04e9aae53efe682bee55c41665757aed
Glitch (camogen) <none>
2020-12-23 22:19:48 +0000

    👭📙 Checkpoint ./src/components/panels/shaders/SketchInputComponent.js:24872531/48401

--- src/components/panels/shaders/SketchInputComponent.js
+++ src/components/panels/shaders/SketchInputComponent.js
@@ -1,5 +1,11 @@
 import { observer } from "mobx-react";
-import React, { useRef, useEffect, useLayoutEffect, useState, useContext } from "react";
+import React, {
+  useRef,
+  useEffect,
+  useLayoutEffect,
+  useState,
+  useContext
+} from "react";
 import {
   ControlGroupComponent,
   InputFloat,
@@ -7,43 +13,95 @@ import {
   InputColor
 } from "maco-ui";
 import MainContext from "../../../MainContext";
-import styles from './SketchInputComponent.module.css';
+import styles from "./SketchInputComponent.module.css";
 import { getSnapshot } from "mobx-state-tree";
-
+let isDrawing = false;
 const SketchInputComponent = observer(props => {
   const store = useContext(MainContext).store;
   const { data } = props.data;
- 
+  const [drawing, setDrawing] = useState(false);
+
   const handleBrushSizeChange = e => data.setBrushSize(e);
-  const handleBrushColorChange = e => data.setBrushColor(e);  
-  
-  const handleMouseMove = e => console.log('mouse event', e)
-  
+  const handleBrushColorChange = e => data.setBrushColor(e);
+
+  const handleMoveStart = (e, wrapper_element) => {
+    /*
+			there are issues with drag events in firefox that make
+			the native 'drag' events less useful for this case. my 
+			alternative was to use mouseEvents and create listeners 
+			on the document
+		*/
+
+    function handleMove(e) {
+      if (e.touches) e = e.touches[0];
+
+      if (e.pageY) {
+        const x = e.pageX - dragOff[0];
+        const y = e.pageY - dragOff[1];
+
+        // limits to upper left
+        props.onPositionChange([x >= 0 ? x : 0, y >= 0 ? y : 0]);
+      }
+    }
+
+    function handleMoveEnd(e) {
+      if (e.touches && e.touches[0]) e = e.touches[0];
+
+      if (e.pageY) {
+        const x = e.pageX - dragOff[0];
+        const y = e.pageY - dragOff[1];
+
+        // limits to upper left
+        props.onPositionChange([x >= 0 ? x : 0, y >= 0 ? y : 0]);
+      }
+
+      document.removeEventListener("mousemove", handleMove);
+      document.removeEventListener("mouseup", handleMoveEnd);
+      document.removeEventListener("touchmove", handleMove);
+      document.removeEventListener("touchend", handleMoveEnd);
+    }
+
+    // use first touch event if on mobile device
+    if (e.touches) e = e.touches[0];
+
+    const p_bounds = wrapper_element.current.getBoundingClientRect();
+    let offset = { x: p_bounds.left, y: p_bounds.top };
+
+    let dragOff = [e.pageX - offset.x, e.pageY - offset.y];
+
+    document.addEventListener("mousemove", handleMove);
+    document.addEventListener("mouseup", handleMoveEnd);
+    document.addEventListener("touchmove", handleMove);
+    document.addEventListener("touchend", handleMoveEnd);
+  };
+
   useEffect(() => {
-    // add event listener for mouse on canvas
-    // console.log(store.getCanvas())
-    console.log(document.getElementById("canvastest"))
-    // return 
-    
     // temp, shouldn't rely on this single ID'd canvas
     let canvas = document.getElementById("canvastest");
-    
-    canvas.addEventListener('mousedown', (e) => handleMouseMove(e));
-    
-    return canvas.removeEventListener('mousedown', (e) => handleMouseMove(e)); 
-  }, [])
+
+    // add event listeners for mouse on canvas
+    canvas.addEventListener("mousedown", e => handleMouseDown(e, canvas));
+    // canvas.addEventListener("mousemove", e => handleMouseMove(e));
+    // canvas.addEventListener("mouseup", e => handleMouseUp(e));
+
+    return () => {
+      canvas.removeEventListener("mousedown", e => handleMouseDown(e, canvas));
+      // canvas.removeEventListener("mousemove", e => handleMouseMove(e));
+      // canvas.removeEventListener("mouseup", e => handleMouseUp(e));
+    };
+  }, []);
 
   return (
     <React.Fragment>
-      <ControlGroupComponent name="Options">        
-        <InputFloat 
-          label='size'
-          value={data.brushSize} 
+      <ControlGroupComponent name="Options">
+        <InputFloat
+          label="size"
+          value={data.brushSize}
           onChange={handleBrushSizeChange}
         />
-        <InputColor 
-          label='color'
-          value={data.brushColor} 
+        <InputColor
+          label="color"
+          value={data.brushColor}
           onChange={handleBrushColorChange}
         />
       </ControlGroupComponent>
