7999d8a305882ab98219171547bc339fe323e3cd
Glitch (camogen) <none>
2020-12-24 00:16:40 +0000

    🙆🐥 Checkpoint ./src/components/panels/shaders/SketchInputComponent.js:24872531/734 ./src/stores/SceneStore.js:24872531/91

--- src/components/panels/shaders/SketchInputComponent.js
+++ src/components/panels/shaders/SketchInputComponent.js
@@ -29,19 +29,11 @@ const SketchInputComponent = observer(props => {
     function handleMove(e) {
       if (e.touches) e = e.touches[0];
 
-      if (e.pageY) {
-        // const x = e.pageX - dragOff[0];
-        // const y = e.pageY - dragOff[1];
-        
-        console.log('moving', [x,y])
-        
+      if (e.pageY) {        
         data.drawLine(x, y, e.offsetX, e.offsetY);
         
         x = e.offsetX;
         y = e.offsetY;
-
-        // limits to upper left
-        // props.onPositionChange([x >= 0 ? x : 0, y >= 0 ? y : 0]);
       }
     }
 
@@ -49,14 +41,8 @@ const SketchInputComponent = observer(props => {
       if (e.touches && e.touches[0]) e = e.touches[0];
 
       if (e.pageY) {
-        // const x = e.pageX - dragOff[0];
-        // const y = e.pageY - dragOff[1];
-
-        console.log('end')
         x = 0;
         y = 0;
-        // limits to upper left
-        // props.onPositionChange([x >= 0 ? x : 0, y >= 0 ? y : 0]);
       }
 
       document.removeEventListener("mousemove", handleMove);
@@ -82,16 +68,10 @@ const SketchInputComponent = observer(props => {
   useEffect(() => {
     // temp, shouldn't rely on this single ID'd canvas
     let canvas = document.getElementById("canvastest");
-
     // add event listeners for mouse on canvas
     canvas.addEventListener("mousedown", e => handleMouseDown(e, canvas));
-    // canvas.addEventListener("mousemove", e => handleMouseMove(e));
-    // canvas.addEventListener("mouseup", e => handleMouseUp(e));
-
     return () => {
       canvas.removeEventListener("mousedown", e => handleMouseDown(e, canvas));
-      // canvas.removeEventListener("mousemove", e => handleMouseMove(e));
-      // canvas.removeEventListener("mouseup", e => handleMouseUp(e));
     };
   }, []);
 
