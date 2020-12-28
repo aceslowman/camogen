80882b71b43a62ad3300be067cb37882aab73ed8
Glitch (camogen) <none>
2020-12-28 23:13:37 +0000

    🌌🎳 Checkpoint ./package.json:24872531/2 ./src/components/panels/HelpComponent.js:24872531/10138

--- src/components/panels/HelpComponent.js
+++ src/components/panels/HelpComponent.js
@@ -1,47 +1,51 @@
 import React from "react";
-import { TextComponent, GenericPanel } from "maco-ui";
+import { TextComponent, GenericPanel, PagesContainer } from "maco-ui";
 
 const Help = props => {
   return (
     <GenericPanel panel={props.panel}>
-      <TextComponent>
-        <h1>camogen</h1>
-        <p>
-          hello! this is <span style={{ color: "orange" }}>camogen</span>, an
-          application that allows you to code, compose, tweak, and record visual
-          effects. It is in beta and not all planned features will be present.
-        </p>
-        <h2>add effects</h2>
-        <ol>
-          <li>
-            under <em>graph</em>, right click on the 'next' node. select Library
-            -> Color -> 2HSV
-          </li>
-        </ol>
-        <h2>change parameters</h2>
-        <ol>
-          <li>
-            under 'controls', click and drag on any of the parameters to change
-            them.
-          </li>
-        </ol>
+      <PagesContainer>
+        <TextComponent>
+          <h1>camogen</h1>
+          <p>
+            hello! this is <span style={{ color: "orange" }}>camogen</span>, an
+            application that allows you to code, compose, tweak, and record visual
+            effects. It is in beta and not all planned features will be present.
+          </p>          
+        </TextComponent>
+        <TextComponent>
+          <h2>add effects</h2>
+          <ol>
+            <li>
+              under <em>graph</em>, right click on the 'next' node. select Library
+              -> Color -> 2HSV
+            </li>
+          </ol>
+          <h2>change parameters</h2>
+          <ol>
+            <li>
+              under 'controls', click and drag on any of the parameters to change
+              them.
+            </li>
+          </ol>
 
-        <h2>add motion</h2>
-        <ol>
-          <li>
-            under <em>graph</em>, click 'Glyph'. this will put the Glyph effect
-            in focus so it can be edited.
-          </li>
-          <li>
-            in the Glyph controls, under 'Shader Controls', right click on the
-            'x' input under 'scale' and click 'edit parameter'
-          </li>
-          <li>under 'Editor', click 'Counter'</li>
-        </ol>
+          <h2>add motion</h2>
+          <ol>
+            <li>
+              under <em>graph</em>, click 'Glyph'. this will put the Glyph effect
+              in focus so it can be edited.
+            </li>
+            <li>
+              in the Glyph controls, under 'Shader Controls', right click on the
+              'x' input under 'scale' and click 'edit parameter'
+            </li>
+            <li>under 'Editor', click 'Counter'</li>
+          </ol>
 
-        <h2>Roadmap</h2>
-        <p></p>
-      </TextComponent>
+          <h2>Roadmap</h2>
+          <p></p>
+        </TextComponent>
+      </PagesContainer>      
     </GenericPanel>
   );
 };
