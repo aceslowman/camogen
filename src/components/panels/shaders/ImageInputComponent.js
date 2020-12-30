4f6417f667b747e47f085fdeebc697c2d09839e8
Glitch (camogen) <none>
2020-12-30 17:57:04 +0000

    👲💎 Checkpoint ./src/components/panels/shaders/ImageInputComponent.js:24872531/830 ./src/components/panels/shaders/ImageInputComponent.module.css:24872531/70

--- src/components/panels/shaders/ImageInputComponent.js
+++ src/components/panels/shaders/ImageInputComponent.js
@@ -1,9 +1,11 @@
 import { observer } from "mobx-react";
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { ControlGroupComponent, InputSelect, InputFloat } from "maco-ui";
+import styles from "./ImageInputComponent.module.css";
 
 const ImageInputComponent = observer(props => {
   const { data } = props.data;
+  const canvas_ref = useRef(null);
 
   const handleFileSubmit = e => {
     data.loadImage(e);
@@ -12,10 +14,17 @@ const ImageInputComponent = observer(props => {
   const handleDisplayMode = e => data.setDisplayMode(e);
   const handlePan = (param, v) => param.setValue(v);
   
-  useLayoutAffect
+  useLayoutEffect(() => {
+    console.log('file changed', data.img)
+    const ctx = canvas_ref.current.getContext("2d");
+    ctx.drawImage(data.img.canvas, 0, 0)
+    canvas_ref.current.width = 80;
+    canvas_ref.current.height = 80;
+  }, [data.img]);
 
   return (
     <React.Fragment>
+      <canvas ref={canvas_ref} className={styles.canvas} />
       <ControlGroupComponent name="Image File">
         <input type="file" onChange={handleFileSubmit} />
       </ControlGroupComponent>
