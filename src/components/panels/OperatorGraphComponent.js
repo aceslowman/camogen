e0ec46eff5b90dcd53a446c3e1a34d6ed8a00e3d
Glitch (camogen) <none>
2021-01-22 04:45:35 +0000

    🌓🚍 Checkpoint ./src/components/panels/OperatorGraphComponent.js:24872531/4625

--- src/components/panels/OperatorGraphComponent.js
+++ src/components/panels/OperatorGraphComponent.js
@@ -1,75 +1,90 @@
-import React, { useContext, useState } from 'react';
-import MainContext from '../../MainContext';
-import GraphComponent from '../graph/GraphComponent';
+import React, { useContext, useState } from "react";
+import MainContext from "../../MainContext";
+import GraphComponent from "../graph/GraphComponent";
 
-import { PanelComponent } from 'maco-ui';
-import { observer } from 'mobx-react';
-import useKeymap from '../hooks/UseKeymap';
+import { PanelComponent } from "maco-ui";
+import { observer } from "mobx-react";
+import useKeymap from "../hooks/UseKeymap";
 
-const OperatorGraph = observer((props) => {
-	const store = useContext(MainContext).store;
-	const [useKeys, setUseKeys] = useState(false);
+const OperatorGraph = observer(props => {
+  const store = useContext(MainContext).store;
+  const [useKeys, setUseKeys] = useState(false);
 
-	useKeymap({
-		"ArrowDown": () => {
-			if (props.selectedNode && props.selectedNode.children.length)
-				props.selectedNode.children[0].select()
-		},
-		"ArrowLeft": () => {
-			if (props.selectedNode && props.selectedNode.children.length) {
-				let idx = props.selectedNode.children[0].parents.indexOf(props.selectedNode);
-				idx--;
+  useKeymap(
+    {
+      ArrowDown: () => {
+        if (props.selectedNode && props.selectedNode.children.length)
+          props.selectedNode.children[0].select();
+      },
+      ArrowLeft: () => {
+        if (props.selectedNode && props.selectedNode.children.length) {
+          let idx = props.selectedNode.children[0].parents.indexOf(
+            props.selectedNode
+          );
+          idx--;
 
-				if (idx >= 0) {
-					props.selectedNode.children[0].parents[idx].select();
-				}
-			}
-		},
-		"ArrowRight": () => {
-			if (props.selectedNode && props.selectedNode.children.length) {
-				let idx = props.selectedNode.children[0].parents.indexOf(props.selectedNode);
-				idx++;
+          if (idx >= 0) {
+            props.selectedNode.children[0].parents[idx].select();
+          }
+        }
+      },
+      ArrowRight: () => {
+        if (props.selectedNode && props.selectedNode.children.length) {
+          let idx = props.selectedNode.children[0].parents.indexOf(
+            props.selectedNode
+          );
+          idx++;
 
-				if (idx <= props.selectedNode.children[0].parents.length - 1)
-					props.selectedNode.children[0].parents[idx].select();
-			}
-		},
-		"ArrowUp": () => {
-			if (props.selectedNode && props.selectedNode.parents.length)
-				props.selectedNode.parents[0].select()
-		},
-		"Delete": () => {
-			props.data.graph.removeSelected();
-		}
-	}, useKeys)
+          if (idx <= props.selectedNode.children[0].parents.length - 1)
+            props.selectedNode.children[0].parents[idx].select();
+        }
+      },
+      ArrowUp: () => {
+        if (props.selectedNode && props.selectedNode.parents.length)
+          props.selectedNode.parents[0].select();
+      },
+      Delete: () => {
+        props.data.graph.removeSelected();
+      }
+    },
+    useKeys
+  );
 
-	const handleFocus = (e) => {
-		setUseKeys(e ? true : false);
-	}
+  const handleFocus = e => {
+    setUseKeys(e ? true : false);
+  };
 
-	return(
-		<PanelComponent 
-			detachable
-			onDetach={props.onDetach ? props.onDetach : () => {}}
-			collapsed={props.collapsed}
-			//title="Operator Graph"				
-			onRemove={()=>store.workspace.removePanel('Operator Graph')}
-			defaultSize={props.defaultSize}
-			onFocus={handleFocus}
-			indicators={useKeys ? [
-				{label:'k', color: store.ui.theme.accent_color, title: 'Keybind Focus'}
-			] : null}
-		>				
-			<GraphComponent 
-				data={props.data.graph}
-				coord_bounds={props.coord_bounds}
-				selectedNode={props.selectedNode}
+  return (
+    <PanelComponent
+      detachable
+      onDetach={props.onDetach ? props.onDetach : () => {}}
+      collapsed={props.collapsed}
+      //title="Operator Graph"
+      onRemove={() => store.workspace.removePanel("Operator Graph")}
+      defaultSize={props.defaultSize}
+      onFocus={handleFocus}
+      indicators={
+        useKeys
+          ? [
+              {
+                label: "k",
+                color: store.ui.theme.accent_color,
+                title: "Keybind Focus"
+              }
+            ]
+          : null
+      }
+    >
+      <GraphComponent
+        data={props.data.graph}
+        coord_bounds={props.coord_bounds}
+        selectedNode={props.selectedNode}
         onContextMenu={props.onContextMenu}
-			/>
+      />
 
-			{ props.data && props.data.updateFlag }
-		</PanelComponent>
-	)  
+      {props.data && props.data.updateFlag}
+    </PanelComponent>
+  );
 });
 
 export default OperatorGraph;
