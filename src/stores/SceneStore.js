96d8b3b1dbf64e87234bd5d3bb112ed212861f19
Glitch (camogen) <none>
2021-01-24 03:59:28 +0000

    🏂🍕 Checkpoint ./src/stores/RootStore.js:24872531/214 ./src/stores/SceneStore.js:24872531/182 ./src/stores/OperatorGraphStore.js:24872531/30

--- src/stores/SceneStore.js
+++ src/stores/SceneStore.js
@@ -57,8 +57,9 @@ const Scene = types
 
     clear: () => {
       self.shaderGraph.clear();
-      self.operatorGraphs.clear();
       self.targets = [];
+      
+      // self.operatorGraphs.clear();
     }
   }));
 
