4b6cad9f33fd3a4186d1c6da16eb15994d871d86
Glitch (camogen) <none>
2021-01-26 05:46:48 +0000

    🐝💃 Checkpoint ./src/stores/shaders/inputs/ImageInput.js:24872531/15949 ./src/stores/shaders/inputs/WebcamInput.js:24872531/15437

--- src/stores/shaders/inputs/ImageInput.js
+++ src/stores/shaders/inputs/ImageInput.js
@@ -1,8 +1,8 @@
 import { getRoot, types, flow } from "mobx-state-tree";
-import Shader, {Uniform} from "../../ShaderStore";
+import Shader, { Uniform } from "../../ShaderStore";
 import * as DefaultShader from "../defaults/DefaultShader";
 import { nanoid } from "nanoid";
-import Parameter from '../../ParameterStore';
+import Parameter from "../../ParameterStore";
 
 const webcam = types
   .model("Image", {
@@ -65,113 +65,106 @@ const webcam = types
   }))
   .actions(self => {
     let root_store;
-
-    function afterAttach() {
-      console.log("attached image input");
-      root_store = getRoot(self);
-    }
-    
-    function beforeDestroy() {      
-      // revoke previous url!p      
-      if(self.dataURL) URL.revokeObjectURL(self.dataURL);
-    }
-
-    function init() {
-      self.ref = self.target.ref.createShader(self.vertex, self.fragment);
-
-      self.extractUniforms();
-
-      for (let uniform of self.uniforms) {
-        self.ref.setUniform(uniform.name, uniform.elements);
-
-        for (let param of uniform.elements) {
-          if (param.graph) self.parameter_graphs.push(param.graph);
-        }
-      }
-
-      let p = root_store.p5_instance;
-      
-      // "images/muybridge.jpg" also works
-      self.img = p.loadImage("images/checkerboard1024.png");
-      self.setImageURL("images/checkerboard1024.png");
-      // prevents init() from being called twice
-      self.ready = true;
-
-      // removes 'tex0' from inputs, since it's provided
-      // by the webcam stream.
-      self.inputs = [];
-    }
-
-    function loadImage(file) {
-      // revoke previous url!    
-      if(self.dataURL) URL.revokeObjectURL(self.dataURL);
-      
-      if (!file.type.startsWith("image/")) return;
-
-      var reader = new FileReader();
-
-      reader.onload = e => {
-        var image = document.createElement("img");
-        let p = root_store.p5_instance;
-        self.setImageURL(e.target.result)
-        self.setImage(p.loadImage(e.target.result));
-      };
-
-      reader.readAsDataURL(file);
-      self.dataURL = URL.createObjectURL(file);
-      console.log('URL.createObjectURL()', URL.createObjectURL(file))
-    }
-
-    function setImage(img) {
-      self.img = img;
-    }
-
-    function setImageURL(img_url) {
-      self.image_url = img_url;
-    }
-    
-    function setDisplayMode(mode) {
-      self.display_mode = mode;
-    }
-
-    function update(p) {
-      let shader = self.ref;
-      let target = self.target.ref;
-
-      for (let uniform_data of self.uniforms) {
-        if (uniform_data.elements.length > 1) {
-          let elements = uniform_data.elements.map(e => e.value);
-          shader.setUniform(uniform_data.name, elements);
-        } else {
-          shader.setUniform(uniform_data.name, uniform_data.elements[0].value);
-        }
-      }
-
-      shader.setUniform("tex0", self.img);
-      shader.setUniform("resolution", [target.width, target.height]);
-      shader.setUniform("img_dimensions", [self.img.width, self.img.height]);
-      shader.setUniform("display_mode", self.displayModeId);
-
-      target.shader(shader);
-
-      try {
-        target.quad(-1, -1, 1, -1, 1, 1, -1, 1);
-      } catch (error) {
-        console.error(error);
-        console.log("frag", shader);
-        p.noLoop();
-      }
-    }
-
     return {
-      afterAttach,
-      beforeDestroy,
-      init,
-      update,
-      setDisplayMode,
-      loadImage,
-      setImage,
-      setImageURL
+      afterAttach: () => {
+        console.log("attached image input");
+        root_store = getRoot(self);
+      },
+
+      beforeDestroy: () => {
+        // revoke previous url!
+        if (self.dataURL) URL.revokeObjectURL(self.dataURL);
+      },
+
+      init: () => {
+        self.ref = self.target.ref.createShader(self.vertex, self.fragment);
+
+        self.extractUniforms();
+
+        for (let uniform of self.uniforms) {
+          self.ref.setUniform(uniform.name, uniform.elements);
+
+          for (let param of uniform.elements) {
+            if (param.graph) self.parameter_graphs.push(param.graph);
+          }
+        }
+
+        let p = root_store.p5_instance;
+
+        // "images/muybridge.jpg" also works
+        self.img = p.loadImage("images/checkerboard1024.png");
+        self.setImageURL("images/checkerboard1024.png");
+        // prevents init() from being called twice
+        self.ready = true;
+
+        // removes 'tex0' from inputs, since it's provided
+        // by the webcam stream.
+        self.inputs = [];
+      },
+
+      loadImage: file => {
+        // revoke previous url!
+        if (self.dataURL) URL.revokeObjectURL(self.dataURL);
+
+        if (!file.type.startsWith("image/")) return;
+
+        var reader = new FileReader();
+
+        reader.onload = e => {
+          var image = document.createElement("img");
+          let p = root_store.p5_instance;
+          self.setImageURL(e.target.result);
+          self.setImage(p.loadImage(e.target.result));
+        };
+
+        reader.readAsDataURL(file);
+        self.dataURL = URL.createObjectURL(file);
+        console.log("URL.createObjectURL()", URL.createObjectURL(file));
+      },
+
+      setImage: img => {
+        self.img = img;
+      },
+
+      setImageURL: img_url => {
+        self.image_url = img_url;
+      },
+
+      setDisplayMode: mode => {
+        self.display_mode = mode;
+      },
+
+      update: p => {
+        let shader = self.ref;
+        let target = self.target.ref;
+
+        for (let uniform_data of self.uniforms) {
+          if (uniform_data.elements.length > 1) {
+            let elements = uniform_data.elements.map(e => e.value);
+            shader.setUniform(uniform_data.name, elements);
+          } else {
+            shader.setUniform(
+              uniform_data.name,
+              uniform_data.elements[0].value
+            );
+          }
+        }
+
+        shader.setUniform("tex0", self.img);
+        shader.setUniform("resolution", [target.width, target.height]);
+        shader.setUniform("img_dimensions", [self.img.width, self.img.height]);
+        shader.setUniform("display_mode", self.displayModeId);
+
+        target.shader(shader);
+
+        try {
+          target.quad(-1, -1, 1, -1, 1, 1, -1, 1);
+        } catch (error) {
+          console.error(error);
+          console.log("frag", shader);
+          p.noLoop();
+        }
+      }
     };
   });
 
