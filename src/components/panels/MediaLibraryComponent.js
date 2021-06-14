b87854d0b7ee6e0e55ccf3bb040dc8bd87a8e9f0
Glitch (camogen) <none>
2021-06-14 18:00:51 +0000

    ⛺️🚥 Checkpoint ./src/components/panels/MediaLibraryComponent.js:24872531/3253 ./src/components/panels/MediaLibraryComponent.module.css:24872531/30

--- src/components/panels/MediaLibraryComponent.js
+++ src/components/panels/MediaLibraryComponent.js
@@ -5,8 +5,10 @@ import { GenericPanel, TextComponent } from "maco-ui";
 import { observer } from "mobx-react";
 import { getSnapshot } from "mobx-state-tree";
 import style from "./MediaLibraryComponent.module.css";
+import { ThemeContext } from 'maco-ui';
 
 const MediaLibrary = observer(props => {
+  const theme = useContext(ThemeContext);
   const store = useContext(MainContext).store;
   const data = store.mediaLibrary;
   const mainRef = useRef();
@@ -14,97 +16,24 @@ const MediaLibrary = observer(props => {
   const [previewSize, setPreviewSize] = useState(100);
   
   const generatePreviews = () => {
+    let tmp = ["","","",""];
     
+    return tmp.map((e,i) => (
+      <div key={i} style={{border: `1px solid ${theme.outline_color}`}}>
+        <div className={style.imageContainer}>
+          <img src="https://via.placeholder.com/150x150" />
+        </div>
+
+        <p>placeholder.png</p>
+      </div>
+    ))
   }
 
   return (
     <GenericPanel panel={props.panel}>
       <div className={style.wrapper}>
         <div className={style.itemPreviewGrid}>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/250x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x250" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/350x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x350" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/250x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x250" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/350x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x350" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
-          <div>
-            <div className={style.imageContainer}>
-              <img src="https://via.placeholder.com/150x150" />
-            </div>
-
-            <p>placeholder.png</p>
-          </div>
+          {generatePreviews()}
         </div>
         <div className={style.itemInfo}>
           <TextComponent>some basic info</TextComponent>
