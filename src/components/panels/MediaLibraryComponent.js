665bd30b0efe51b9e3d6659d3f5875c25f81747f
Glitch (camogen) <none>
2021-06-14 15:42:08 +0000

    🐙🐈 Checkpoint ./src/components/panels/MediaLibraryComponent.module.css:24872531/512 ./src/components/panels/MediaLibraryComponent.js:24872531/9063

--- src/components/panels/MediaLibraryComponent.js
+++ src/components/panels/MediaLibraryComponent.js
@@ -10,70 +10,102 @@ const MediaLibrary = observer(props => {
   const store = useContext(MainContext).store;
   const data = store.mediaLibrary;
   const mainRef = useRef();
-  
+
   const [previewSize, setPreviewSize] = useState(100);
 
   return (
-    <GenericPanel
-      panel={props.panel}    
-    >
+    <GenericPanel panel={props.panel}>
       <div className={style.wrapper}>
         <div className={style.itemPreviewGrid}>
           <div>
-            <img src="https://via.placeholder.com/150/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/250/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/250x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/250"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x250" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/350/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/350x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/350"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x350" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/250/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/250x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/250"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x250" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/350/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/350x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/350"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x350" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/150/150"/>
+            <div className={style.imageContainer}>
+              <img src="https://via.placeholder.com/150x150" />
+            </div>
+
             <p>placeholder.png</p>
           </div>
         </div>
         <div className={style.itemInfo}>
-          <TextComponent>
-            some basic info
-          </TextComponent>          
+          <TextComponent>some basic info</TextComponent>
         </div>
-      </div>      
+      </div>
     </GenericPanel>
   );
 });
