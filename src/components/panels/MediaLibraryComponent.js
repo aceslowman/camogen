74a912e426def07283ab81ae787f13b860421c01
Glitch (camogen) <none>
2021-06-16 01:38:29 +0000

    🐂🏤 Checkpoint ./src/components/panels/MediaLibraryComponent.js:24872531/6562

--- src/components/panels/MediaLibraryComponent.js
+++ src/components/panels/MediaLibraryComponent.js
@@ -13,17 +13,13 @@ const MediaLibrary = observer(props => {
   const store = useContext(MainContext).store;
   const data = store.mediaLibrary;
   const mainRef = useRef();
+  let previews;
 
   const [previewSize, setPreviewSize] = useState(100);
-  
-  useEffect(() => {
-    console.log('store', getSnapshot(store.mediaLibrary.media))
-  }, [store.mediaLibrary,store.mediaLibrary.media.size])
 
-  const generatePreviews = () => {
-    let tmp = ["", "", "", ""];    
-
-    return tmp.map((e, i) => (
+  const generatePreviews = useEffect(() => {
+    console.log("store", getSnapshot(store.mediaLibrary.media));
+    previews = Array.from(store.mediaLibrary.media).map((e, i) => (
       <div
         key={i}
         style={{
@@ -39,10 +35,31 @@ const MediaLibrary = observer(props => {
         <div className={style.imageName}>placeholder.png</div>
       </div>
     ));
-  };
+  }, [store.mediaLibrary, store.mediaLibrary.media.size]);
 
-  const handleDrop = e => {    
-    console.log(store)
+  //   const generatePreviews = () => {
+  //     let tmp = ["", "", "", ""];
+
+  //     return tmp.map((e, i) => (
+  //       <div
+  //         key={i}
+  //         style={{
+  //           border: `1px solid ${theme.outline_color}`,
+  //           backgroundColor: theme.primary_color
+  //         }}
+  //       >
+  //         <div className={style.imageContainer}>
+  //           {/* TODO: should generate thumbnails */}
+  //           <img src="https://via.placeholder.com/150x150" />
+  //         </div>
+
+  //         <div className={style.imageName}>placeholder.png</div>
+  //       </div>
+  //     ));
+  //   };
+
+  const handleDrop = e => {
+    console.log(store);
     store.mediaLibrary.addMedia(e);
   };
 
@@ -57,11 +74,11 @@ const MediaLibrary = observer(props => {
               }}
               className={style.itemPreviewGrid}
               style={{
-                backgroundColor: theme.secondary_color,
+                backgroundColor: theme.secondary_color
                 // border: `1px dotted ${theme.text_color}`
               }}
             >
-              {generatePreviews()}
+              {previews}
             </div>
           )}
         </Dropzone>
