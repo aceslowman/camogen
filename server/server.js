33cea8d4c0e5aace98916594cdda769b62e8495c
Glitch (feather-darkened-sombrero) <none>
2023-10-08 18:00:26 +0000

    ⚾️🎙 Checkpoint ./package.json:24872531/6125 ./server/server.js:24872531/5169

--- server/server.js
+++ server/server.js
@@ -1,73 +1,77 @@
 const express = require("express");
 const path = require("path");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const fs = require("fs");
-const dirTree = require('directory-tree');
-const app = express(); 
-const { nanoid } = require('nanoid');
- 
-let shader_collection;   
-  
-/* TODO: should rely on camo.collection files */
-function preloadDefaultShaders() {
-  const shader_path = path.resolve(__dirname, '../shaders');
-   
-  shader_collection = dirTree(shader_path, {}, 
-    file => {    // files    
-      file.id = nanoid();
-      fs.readFile(file.path, 'utf8', (err, data) => {        
-        if(err) {
-          console.error(err);  
-        } else {  
-          file.data = JSON.parse(data);
-        } 
-      })       
-    },
-    dir => {    // directories
-      dir.id = nanoid(); 
-    },
-  );  
-   
-  shader_collection.id = 'default-collection_'+nanoid();
-}   
+const dirTree = require("directory-tree");
+const app = express();
+const { nanoid } = require("nanoid");
 
-preloadDefaultShaders();
+// let shader_collection;
 
-function checkHttps(req, res, next){
-  // protocol check, if http, redirect to https
-  
-  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
-    return next()
-  } else {
-    res.redirect('https://' + req.hostname + req.url);
-  }
-}
+// /* TODO: should rely on camo.collection files */
+// function preloadDefaultShaders() {
+//   const shader_path = path.resolve(__dirname, '../shaders');
 
-app.all("*", checkHttps);
+//   shader_collection = dirTree(shader_path, {},
+//     file => {    // files
+//       file.id = nanoid();
+//       fs.readFile(file.path, 'utf8', (err, data) => {
+//         if(err) {
+//           console.error(err);
+//         } else {
+//           file.data = JSON.parse(data);
+//         }
+//       })
+//     },
+//     dir => {    // directories
+//       dir.id = nanoid();
+//     },
+//   );
 
-// A test route to make sure the server is up.
-app.get("/api/data", (request, response) => {
-  console.log("❇️ Received GET request to /api/data");
-  response.json({"message": "hello world"});
-});
+//   shader_collection.id = 'default-collection_'+nanoid();
+// }
 
-app.get("/api/shaders", (request, response) => {
-  response.json(shader_collection);
-});
+// preloadDefaultShaders();
+
+// function checkHttps(req, res, next) {
+//   // protocol check, if http, redirect to https
+
+//   if (req.get("X-Forwarded-Proto").indexOf("https") != -1) {
+//     return next();
+//   } else {
+//     res.redirect("https://" + req.hostname + req.url);
+//   }
+// }
+
+// app.all("*", checkHttps);
+
+// // A test route to make sure the server is up.
+// app.get("/api/data", (request, response) => {
+//   console.log("❇️ Received GET request to /api/data");
+//   response.json({"message": "hello world"});
+// });
+
+// app.get("/api/shaders", (request, response) => {
+//   response.json(shader_collection);
+// });
 
 // Express port-switching logic
-let port;
-console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
-if (process.env.NODE_ENV === "production") {
-  port = process.env.PORT || 3000;
-  app.use(express.static(path.join(__dirname, "../build")));
-  app.use(express.static(path.join(__dirname, "../public")));
-  app.get("*", (request, response) => {
-    response.sendFile(path.join(__dirname, "../build", "index.html"));
-  });
-} else {
-  port = 3001;
-}
+// let port;
+// console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
+// if (process.env.NODE_ENV === "production") {
+//   port = process.env.PORT || 3000;
+//   app.use(express.static(path.join(__dirname, "../build")));
+//   app.use(express.static(path.join(__dirname, "../public")));
+//   app.get("*", (request, response) => {
+//     response.sendFile(path.join(__dirname, "../build", "index.html"));
+//   });
+// } else {
+//   port = 3001;
+// }
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
 // proxy is setup both here and in the webpack devServer config
 // app.use('/api', createProxyMiddleware(
@@ -78,6 +82,6 @@ if (process.env.NODE_ENV === "production") {
 // ));
 
 // Start the listener!
-const listener = app.listen(port, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log("❇️ Express server is running on port", listener.address().port);
 });
